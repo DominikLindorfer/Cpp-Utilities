e6a13fdeed85963bbe32105f65684c24374883f0
lindorfer <dominik.lindorfer@jku.at>
2020-09-08 10:25:14 +0200

    Added move_element function for containers

--- Utilities.hpp
+++ Utilities.hpp
@@ -107,3 +107,11 @@ ostream& operator<< (ostream& out, unordered_map<K, T>& umap) {
     return out;
 }
 
+//-----Move Elements within a Container (e.g. Boost or Std Vector)-----
+template <typename vec> void move_element(vec& v, size_t oldIndex, size_t newIndex){
+    if (oldIndex > newIndex)
+        std::rotate(v.rend() - oldIndex - 1, v.rend() - oldIndex, v.rend() - newIndex);
+    else
+        std::rotate(v.begin() + oldIndex, v.begin() + oldIndex + 1, v.begin() + newIndex + 1);
+}
+
